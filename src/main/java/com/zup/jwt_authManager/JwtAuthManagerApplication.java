53ccbc1728cc918ffaace25e6550de111180a44b
LuizFernando <barbosa.luiz@zup.com.br>
2025-02-25 09:55:02 -0300

    chore: commit inicial

---
+++ src/main/java/com/zup/jwt_authManager/JwtAuthManagerApplication.java
@@ -0,0 +1,13 @@
+package com.zup.jwt_authManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zup/jwt_authManager/JwtAuthManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.jwt_authManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
