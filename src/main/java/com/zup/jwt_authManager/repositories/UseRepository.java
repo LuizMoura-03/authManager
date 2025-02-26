f19fdf170983350d05af3d8d2fdf4d896a99a88e
LuizFernando <barbosa.luiz@zup.com.br>
2025-02-25 23:03:08 -0300

    feat: criar a interface useRepository para gerenciar usuarios

---
+++ src/main/java/com/zup/jwt_authManager/repositories/UseRepository.java
@@ -0,0 +1,7 @@
+package com.zup.jwt_authManager.repositories;
+
+import com.zup.jwt_authManager.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UseRepository extends JpaRepository<UserModel, Long> {
+}
