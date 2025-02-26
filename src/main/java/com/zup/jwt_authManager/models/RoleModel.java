7e3a119fe6d0523f91571dbd98d44e5002f02863
LuizFernando <barbosa.luiz@zup.com.br>
2025-02-25 22:56:15 -0300

    feat: criar a classe RoleModel para representar os papeis dos usuarios

--- src/main/java/com/zup/jwt_authManager/models/RoleModel.java
+++ src/main/java/com/zup/jwt_authManager/models/RoleModel.java
@@ -1,4 +1,18 @@
 package com.zup.jwt_authManager.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "roles")
 public class RoleModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
 }
