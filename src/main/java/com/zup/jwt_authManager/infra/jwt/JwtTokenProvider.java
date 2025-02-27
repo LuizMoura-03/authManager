3b1112344540b6defc7ab0321f08e153f17bc69c
LuizFernando <barbosa.luiz@zup.com.br>
2025-02-27 18:24:51 -0300

    feat: adiciona geração e extração de roles e claims no token JWT

--- src/main/java/com/zup/jwt_authManager/infra/jwt/JwtTokenProvider.java
+++ src/main/java/com/zup/jwt_authManager/infra/jwt/JwtTokenProvider.java
@@ -7,18 +7,19 @@ import io.jsonwebtoken.security.Keys;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.Map;
 
 public class JwtTokenProvider {
     private String jwtSecret = "caf60addca9ea3e3c099551e1b6576c9966dce0a33de879dd7e160f86dbd872ca"; // Chave secreta para assinar o token.
     private long jwtExpirationDate = 300000; // Tempo de expiração do token (5 minutos).
 
-    public String generateToken(String username) {
-        // Gera um token JWT
+    public String generateToken(String username, Map<String, Object> claims) {   // Gera um token JWT com roles e claims
         Date currentDate = new Date(); // Data atual.
         Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate); // Data de expiração.
 
         return Jwts.builder()
                 .setSubject(username) // Define o nome do usuário como o assunto do token.
+                .addClaims(claims) // Adiciona claims.
                 .setIssuedAt(currentDate) // Define a data de emissão.
                 .setExpiration(expireDate) // Define a data de expiração.
                 .signWith(SignatureAlgorithm.HS256, key()) // Assina o token com a chave secreta.
@@ -38,6 +39,16 @@ public class JwtTokenProvider {
                 .parseClaimsJws(token) // Analisa o token.
                 .getBody()
                 .getSubject(); // Retorna o nome do usuário.
+    }
+
+    // Extrai uma claim especifica do token JWT.
+    public Object getClaim(String token, String claimKey) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key()) // Define a chave para validação.
+                .build()
+                .parseClaimsJws(token) // Analisa o token.
+                .getBody()
+                .get(claimKey); // Retorna a claim específica.
     }
 
     public boolean validateToken(String token) {
