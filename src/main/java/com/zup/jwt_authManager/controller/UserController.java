618dc9d2dc6bebabec440829a67ef099c5b7d96f
LuizFernando <barbosa.luiz@zup.com.br>
2025-03-06 14:19:07 -0300

    feat: criar AuthResponseDTO para encapsular o token JWT

---
+++ src/main/java/com/zup/jwt_authManager/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.zup.jwt_authManager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class UserController {
+}
---
+++ src/main/java/com/zup/jwt_authManager/controller/dtos/AuthResponseDTO.java
@@ -0,0 +1,11 @@
+package com.zup.jwt_authManager.controller.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponseDTO {
+    private String token;
+
+}
