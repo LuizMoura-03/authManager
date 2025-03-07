068296323bafe60b06d2a2895824864259740db4
LuizFernando <barbosa.luiz@zup.com.br>
2025-03-07 10:27:41 -0300

    feat: adiciona a classe LoginService e  ajustado o metodo generatedToken na JwtTokenProvider

--- src/main/java/com/zup/jwt_authManager/infra/jwt/JwtTokenProvider.java
+++ src/main/java/com/zup/jwt_authManager/infra/jwt/JwtTokenProvider.java
@@ -4,22 +4,28 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import java.security.Key;
 import java.util.Date;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 public class JwtTokenProvider {
     private String jwtSecret = "caf60addca9ea3e3c099551e1b6576c9966dce0a33de879dd7e160f86dbd872ca"; // Chave secreta para assinar o token.
     private long jwtExpirationDate = 300000; // Tempo de expiração do token (5 minutos).
 
-    public String generateToken(String username, Map<String, Object> claims) {   // Gera um token JWT com roles e claims
+    public String generateToken(Authentication authentication) {   // Gera um token JWT com roles e claims
+        String username = authentication.getName();
         Date currentDate = new Date(); // Data atual.
-        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate); // Data de expiração.
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
+
+        String roles = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
 
         return Jwts.builder()
                 .setSubject(username) // Define o nome do usuário como o assunto do token.
-                .addClaims(claims) // Adiciona claims.
+                .claim("roles", roles) // Adiciona claims.
                 .setIssuedAt(currentDate) // Define a data de emissão.
                 .setExpiration(expireDate) // Define a data de expiração.
                 .signWith(SignatureAlgorithm.HS256, key()) // Assina o token com a chave secreta.
@@ -41,16 +47,6 @@ public class JwtTokenProvider {
                 .getSubject(); // Retorna o nome do usuário.
     }
 
-    // Extrai uma claim especifica do token JWT.
-    public Object getClaim(String token, String claimKey) {
-        return Jwts.parserBuilder()
-                .setSigningKey(key()) // Define a chave para validação.
-                .build()
-                .parseClaimsJws(token) // Analisa o token.
-                .getBody()
-                .get(claimKey); // Retorna a claim específica.
-    }
-
     public boolean validateToken(String token) {
         // Valida o token JWT.
         Jwts.parserBuilder()
@@ -59,5 +55,6 @@ public class JwtTokenProvider {
                 .parseClaimsJws(token); // Analisa o token.
         return true; // Retorna true se o token for válido.
     }
+
 }
 
---
+++ src/main/java/com/zup/jwt_authManager/services/LoginService.java
@@ -0,0 +1,32 @@
+package com.zup.jwt_authManager.services;
+
+import com.zup.jwt_authManager.controller.dtos.LoginRequestDTO;
+import com.zup.jwt_authManager.infra.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public String login(LoginRequestDTO loginRequestDTO) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequestDTO.getUsername(),
+                        loginRequestDTO.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return jwtTokenProvider.generateToken(authentication);
+    }
+
+}
